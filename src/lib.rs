e293c14df44824be4e4a1af050a555b5b184b508
Jack Dockerty <jdockerty19@gmail.com>
2025-07-05 23:09:11 +0100

    fix: use channel for dispatcher result

--- src/lib.rs
+++ src/lib.rs
@@ -2,28 +2,55 @@ use std::{net::IpAddr, str::FromStr, time::Duration};
 
 use prometheus::{IntCounter, Registry};
 use surge_ping::{Client, Config, PingIdentifier, PingSequence};
+use tokio::sync::mpsc::{error::TryRecvError, Receiver, Sender};
 
 pub type Result<T, E = Box<dyn std::error::Error + Send + Sync>> = std::result::Result<T, E>;
 
 pub struct PingSender {
-    dispatchers: Vec<Dispatcher>,
+    dispatchers: Vec<(Dispatcher, Receiver<Result<()>>)>,
+
+    success_count: IntCounter,
+    failure_count: IntCounter,
 }
 
 impl PingSender {
     pub fn new(targets: Vec<String>, metrics: &Registry) -> Result<Self> {
+        let success_count = IntCounter::new("ping_success_count", "TODO")?;
+        let failure_count = IntCounter::new("ping_failure_count", "TODO")?;
+        metrics.register(Box::new(success_count.clone()))?;
+        metrics.register(Box::new(failure_count.clone()))?;
         Ok(Self {
             dispatchers: targets
                 .iter()
-                .map(|t| Dispatcher::new(t.clone(), &metrics))
+                .map(|t| Dispatcher::new(t.clone()))
                 .collect::<Result<_>>()?,
+            success_count,
+            failure_count,
         })
     }
 }
 
 /// Start pinging all targets configured within the [`PingSender`]
 pub async fn ping_targets(sender: PingSender) {
-    for d in sender.dispatchers {
-        tokio::spawn(d.run());
+    for (dispatcher, mut rx) in sender.dispatchers {
+        let success_count = sender.success_count.clone();
+        let failure_count = sender.failure_count.clone();
+
+        tokio::spawn(dispatcher.run());
+        tokio::spawn(async move {
+            let mut interval = tokio::time::interval(Duration::from_millis(10));
+            loop {
+                interval.tick().await;
+                match rx.try_recv() {
+                    Ok(res) => match res {
+                        Ok(_) => success_count.inc(),
+                        Err(_) => failure_count.inc(),
+                    },
+                    Err(TryRecvError::Empty) => {}
+                    Err(TryRecvError::Disconnected) => panic!("send disconnected"),
+                }
+            }
+        });
     }
 }
 
@@ -32,27 +59,22 @@ struct Dispatcher {
     target: String,
     client: Client,
 
-    success_count: IntCounter,
-    failure_count: IntCounter,
+    result_tx: Sender<Result<()>>,
 }
 
 impl Dispatcher {
-    fn new(target: String, metrics: &Registry) -> Result<Self> {
+    fn new(target: String) -> Result<(Self, Receiver<Result<()>>)> {
         let client = surge_ping::Client::new(&Config::new())?;
 
-        // TODO: this will fail running multiple.
-        //
-        // Fix this by placing metrics at the top-level for overall successful/failed pings.
-        let success_count = IntCounter::new("dispatcher_success_count", "TODO")?;
-        let failure_count = IntCounter::new("dispatcher_failure_count", "TODO")?;
-        metrics.register(Box::new(success_count.clone())).unwrap();
-        metrics.register(Box::new(failure_count.clone())).unwrap();
-        Ok(Self {
-            target,
-            client,
-            success_count,
-            failure_count,
-        })
+        let (result_tx, result_rx) = tokio::sync::mpsc::channel(100);
+        Ok((
+            Self {
+                target,
+                client,
+                result_tx,
+            },
+            result_rx,
+        ))
     }
 
     /// Run this dispatcher, performing the ping operation to the given target.
@@ -74,11 +96,11 @@ impl Dispatcher {
             match pinger.ping(PingSequence(0), &[]).await {
                 Ok((_, duration)) => {
                     println!("{}: {duration:?}", self.target);
-                    self.success_count.inc();
+                    self.result_tx.send(Ok(())).await?;
                 }
                 Err(e) => {
                     eprintln!("{e}");
-                    self.failure_count.inc()
+                    self.result_tx.send(Err(Box::new(e))).await?;
                 }
             }
         }
