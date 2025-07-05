62351845b5f78d63eeb3d83aecc9f407d118699e
Jack Dockerty <jdockerty19@gmail.com>
2025-07-05 21:03:57 +0100

    feat: add basic ping dispatcher

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,29 @@
+use std::{net::IpAddr, str::FromStr};
 
+use surge_ping::{Client, Config, PingIdentifier, PingSequence};
+
+pub type Result<T, E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
+
+pub struct Dispatcher {
+    target: String,
+    client: Client,
+}
+
+impl Dispatcher {
+    pub fn new(target: String) -> Result<Self> {
+        let client = surge_ping::Client::new(&Config::new())?;
+        Ok(Self { target, client })
+    }
+
+    pub async fn run(self) -> Result<()> {
+        let mut pinger = self
+            .client
+            .pinger(IpAddr::from_str(&self.target)?, PingIdentifier(10))
+            .await;
+
+        loop {
+            let (_, duration) = pinger.ping(PingSequence(0), &[]).await?;
+            println!("{duration:?}");
+        }
+    }
+}
