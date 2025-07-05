bb5480962861bd4eee8adce79b9a408e666bb850
Jack Dockerty <jdockerty19@gmail.com>
2025-07-05 21:42:02 +0100

    feat: add simple metrics

--- src/bin/uppies.rs
+++ src/bin/uppies.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 
+use prometheus::Registry;
 use uppies::{Dispatcher, Result};
 
 #[derive(Debug, Parser)]
@@ -12,11 +13,14 @@ struct Cli {
 async fn main() -> Result<()> {
     let cli = Cli::parse();
 
+    let metrics = Registry::default();
+
     for target in cli.targets {
-        let dispatcher = Dispatcher::new(target.clone())?;
+        let dispatcher = Dispatcher::new(target.clone(), &metrics)?;
         tokio::spawn(dispatcher.run());
     }
 
+    // TODO: include HTTP server to serve metrics path
     tokio::signal::ctrl_c().await?;
 
     Ok(())
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::{net::IpAddr, str::FromStr, time::Duration};
 
+use prometheus::{IntCounter, Registry};
 use surge_ping::{Client, Config, PingIdentifier, PingSequence};
 
 pub type Result<T, E = Box<dyn std::error::Error + Send + Sync>> = std::result::Result<T, E>;
@@ -8,14 +9,31 @@ pub type Result<T, E = Box<dyn std::error::Error + Send + Sync>> = std::result::
 pub struct Dispatcher {
     target: String,
     client: Client,
+
+    success_count: IntCounter,
+    failure_count: IntCounter,
 }
 
 impl Dispatcher {
-    pub fn new(target: String) -> Result<Self> {
+    pub fn new(target: String, metrics: &Registry) -> Result<Self> {
         let client = surge_ping::Client::new(&Config::new())?;
-        Ok(Self { target, client })
+
+        let success_count = IntCounter::new("dispatcher_success_count", "TODO")?;
+        let failure_count = IntCounter::new("dispatcher_failure_count", "TODO")?;
+        metrics.register(Box::new(success_count.clone())).unwrap();
+        metrics.register(Box::new(failure_count.clone())).unwrap();
+        Ok(Self {
+            target,
+            client,
+            success_count,
+            failure_count,
+        })
     }
 
+    /// Run this dispatcher, performing the ping operation to the given target.
+    ///
+    /// This is a blocking call and will perform continuous pings against
+    /// the target.
     pub async fn run(self) -> Result<()> {
         let mut pinger = self
             .client
@@ -28,8 +46,16 @@ impl Dispatcher {
         let mut interval = tokio::time::interval(Duration::from_millis(10));
         loop {
             interval.tick().await;
-            let (_, duration) = pinger.ping(PingSequence(0), &[]).await?;
-            println!("{}: {duration:?}", self.target);
+            match pinger.ping(PingSequence(0), &[]).await {
+                Ok((_, duration)) => {
+                    println!("{}: {duration:?}", self.target);
+                    self.success_count.inc();
+                }
+                Err(e) => {
+                    eprintln!("{e}");
+                    self.failure_count.inc()
+                }
+            }
         }
     }
 }
